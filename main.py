dec7ede3a2cbf1eb59f35129646206623e5ba4ab
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2025-12-29 18:44:41 -0500

    feat: Show usage metadata

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ client = genai.Client(api_key=api_key)
 
 def main():
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if response.usage_metadata is None:
+        raise RuntimeError("The request to Gemini failed.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
