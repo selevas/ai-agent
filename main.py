d5f5b9a46c660b0897a60c667ac7057d85f4b1cb
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2025-12-30 12:06:06 -0500

    feat: Add --verbose command line argument

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if response.usage_metadata is None:
         raise RuntimeError("The request to Gemini failed.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
