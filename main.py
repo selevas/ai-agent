21ee03b6be804b19e3a8770a22153aec0c8cf94a
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2025-12-30 09:12:32 -0500

    feat: Add ability to pass message via cmd line arg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from dotenv import load_dotenv
 
@@ -11,7 +12,10 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if response.usage_metadata is None:
         raise RuntimeError("The request to Gemini failed.")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
