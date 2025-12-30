d24eb2e0f50a214338e2a83b6ac287683de64d97
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2025-12-30 12:06:33 -0500

    feat: Track multiple exchanges of dialogue

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if api_key is None:
     raise RuntimeError("No Gemini API key was found in the environment.")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
@@ -16,7 +17,8 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if response.usage_metadata is None:
         raise RuntimeError("The request to Gemini failed.")
     if args.verbose == True:
