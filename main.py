aa882cd25d8b4a57a4660fec82fc422ea07ff282
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2026-01-03 13:09:22 -0500

    feat: Add get_files_info as callable function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,7 +24,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt,
+        ),
     )
     if response.usage_metadata is None:
         raise RuntimeError("The request to Gemini failed.")
@@ -31,8 +34,12 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    if response.function_calls is None:
+        print("Response:")
+        print(response.text)
+    else:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
 
 if __name__ == "__main__":
     main()
